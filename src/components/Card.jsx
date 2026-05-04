130173b11e7b4938153c61da2ad3eb80377e4b5b
Gustapogomes <gustavo.cabral@nave.org.br>
2026-05-04 14:44:50 -0300

    salvaavavavva

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,68 @@ import Menu from './Menu'
 import Botao from './Botao'
 
 export default function Card() {
-    
-    const codigoThais = <View>
+
+
+    const codigoThais = 
+    <View style={{display: "flex", flexDirection: "row"}}>
+        <View style={{display: "flex", flexDirection: "column"}}>
+            <Text>
+                Sobre a Lista de Laços 
+            </Text>
+            <Text>   </Text>
+            <Text>
+                Uma questão que achou muito fácil: Foi a questão 4 da primira lista de exercicios
+            </Text>
+            <Text>{"let yuta = 0"}</Text>
+                <Text>{"let rika = 0"}</Text>
+                <Text>{"while (rika <= 100) {"}</Text>
+                    <Text>{"  if(rika % 7 ==  0){"}</Text>
+                        <Text>{"    yuta = yuta + rika"}</Text>
+                    <Text>{"  }"}</Text>
+                    <Text>{"  rika++"}</Text>
+                    <Text>{"  console.log(yuta)"}</Text>
+                <Text>{"}"}</Text>
+
+            <Text>       </Text>
+
+            <Text>
+                Uma questão que achou muito difícil: Foi a questão 10 da segunda lista de exercicios
+            </Text>
+            <Text>{"let nagi = [ ]"}</Text>
+            <Text>{"for(let reo of petsExoticos){"}</Text>
+            <Text>{"  if(reo.includes('r')){"}</Text>
+            <Text>{"    nagi.push(reo)"}</Text>
+            <Text>{" }"}</Text>
+ 
+            <Text>{"}"}</Text>
+            <Text>     </Text>
+        </View>
+        <View style={{display: "flex", flexDirection: "column", left: 100, marginTop: "-3px" }}>
+            <Text>  </Text>
+            <Text>  </Text>
+            <Text>
+                Uma questão que o ajudou a entender muita coisa: Foi a questão 8 da segunda lista de exercicios
+            </Text>
+            <Text>{"let bolinha = []"}</Text>
+        <Text>{"for(let cesar = 0; cesar < itensDomesticos.length; cesar++ ){"}</Text>
+            <Text>{"  bolinha.unshift(itensDomesticos[cesar])"}</Text>
+        <Text>{"}"}</Text>
+        <Text>   </Text>
         <Text>
-            Sobre a Lista de Laços 
+            Uma questão que não ajudou a entender nada: Foi a questão 8 da segunda lista de exercicios
         </Text>
+       <Text>{"let somma = 0"}</Text>
+    <Text>{"for(let b = -100; b <= -1; b++){"}</Text>
+    <Text>{"  if(b % 2 === 0){"}</Text>
+    <Text>{"    somma += b"}</Text>
+    <Text>{"  }"}</Text>
+    <Text>{"  //console.log(somma)"}</Text>
+    <Text>{"}"}</Text>
+
+        </View>   
     </View>
+
+
 
     const codigoLaura = <View>
         <Text>
@@ -17,9 +73,12 @@ export default function Card() {
         </Text>
     </View>
 
-       const codigoGustavo = <View>
+    const codigoGustavo = <View>
         <Text>
             Códigos de Thais aqui
+        </Text>
+        <Text>
+            oiiiii
         </Text>
     </View>
     
@@ -27,7 +86,11 @@ export default function Card() {
         <Text>
             Códigos de Thais aqui
         </Text>
+
     </View>
+    
+    
+    
 
     const [alunoSelecionado, setAlunoSelecionado] = useState("Thais")
     const [codigo, setCodigo] = useState(codigoThais)
@@ -62,6 +125,7 @@ export default function Card() {
                 </Text>
 
             </View>
+            
         </View>
     )
 }
